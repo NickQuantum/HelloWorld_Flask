4b9f09472f7e9792dcd38221baedf42e7f9e6ebf
unknown <U23139@E121LLAX.ustr.com>
2015-03-28 21:58:44 -0500

    Hello World Application for AWS first commit

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask
+ 
+application = Flask(__name__)
+
+#Set application.debug=true to enable tracebacks on Beanstalk log output. 
+#Make sure to remove this line before deploying to production.
+application.debug=True
+ 
+@application.route('/')
+def hello():
+    return "Hello world!"
+ 
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
