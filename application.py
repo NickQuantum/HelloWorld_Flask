82f9ce8937552f2d8beb57398b63a1dba19fac80
unknown <U23139@E121LLAX.ustr.com>
2015-03-28 22:32:18 -0500

    Hello World Application for AWS second commit

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ application.debug=True
  
 @application.route('/')
 def hello():
-    return "Hello world!"
+    return "Hello again world!"
  
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
